9b69ae9183e464bc56790b027b651f64a019c197
Jordan Estralioto <estraliotojordan@gmail.com>
2025-10-23 09:19:40 -0300

    Remove unused scripts and functions related to data extraction and folder creation for Brazilian financial data processing. This includes the deletion of Criando_pastas.py, Extraindo_arquivos.py, app.py, funcoes.py, and url_e_nomes.py, streamlining the codebase and eliminating redundancy.

--- src/brazil/dados_b3/Dados_B3_Acoes/Percentual_ibov/__init__.py
+++
@@ -1,3 +0,0 @@
-# Neste código, irei criar uma função para pegar os dados do percentual do IBOV diretamente do site da B3.
-
-# Site: https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/composicao-da-carteira-ibov.htm
--- src/brazil/dados_b3/Opcoes_B3/Calc_Ops/__init__.py
+++
@@ -1,1 +0,0 @@
-# Nesta pasta irei criar o código para calcular o valor justo das opções com base em um modelo de Black-Scholes
